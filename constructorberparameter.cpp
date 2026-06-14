2aae35d261fe2dcde0c71ad2bd4e757949261684
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-06-14 22:45:15 +0700

    define parameterized constructor outside the class using scope resolution

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -10,3 +10,21 @@ public:
 int main() {
     return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(int nim, string nama); // constructor dengan parameter
+};
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+}
+
+int main() {
+    return 0;
+}
