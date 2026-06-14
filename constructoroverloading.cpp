d34d5473beb3caa298677d5469a3a472b3f3b3e7
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-06-14 22:49:18 +0700

    implement default, int-parameter, and string-parameter constructors

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -17,3 +17,34 @@ public:
 int main() {
     return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+int main() {
+    return 0;
+}
