ebff9de147b37c7d8d5180f0c7d0d6bdcadf9b55
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-06-14 22:49:54 +0700

    Implementasi Multi-Parameter Constructor dan Method cetak

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -48,3 +48,44 @@ mahasiswa :: mahasiswa(string iNama){
 int main() {
     return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama= " << nama << endl;
+}
+
+int main() {
+    return 0;
+}
