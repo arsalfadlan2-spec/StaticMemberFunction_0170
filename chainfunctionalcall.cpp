4a72ef4d9a30a116975e241bb5e6295e41e12301
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-06-14 22:40:38 +0700

    menambahkan method setter dengan metode chaining

--- chainfunctionalcall.cpp
+++ chainfunctionalcall.cpp
@@ -27,3 +27,24 @@ public:
 int main() {
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this; // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
+
+int main() {
+    return 0;
+}
